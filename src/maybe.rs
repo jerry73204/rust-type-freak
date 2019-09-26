25e832b066524e38046ab4322b7e78d23e25a877
jerry73204 <jerry73204@gmail.com>
2019-09-26 22:59:57 +0800

    [maybe] Let functor works on Maybe

--- src/maybe.rs
+++ src/maybe.rs
@@ -15,6 +15,7 @@
 //! type Val3 = UnwrapOrOutput<Opt2, U0>; // U0
 //! ```
 
+use crate::functional::{ApplyFunctor, Functor};
 use std::marker::PhantomData;
 
 // maybe def
@@ -75,12 +76,65 @@ impl<DefaultValue> UnwrapOr<DefaultValue> for Nothing {
     type Output = DefaultValue;
 }
 
+// map the value of maybe
+
+/// Maps a [Maybe] type by a [Functor].
+pub trait MaybeMap<Func>
+where
+    Self: Maybe,
+    Self::Output: Maybe,
+{
+    type Output;
+}
+
+pub type MaybeMapOutput<MaybeInput, Func> = <MaybeInput as MaybeMap<Func>>::Output;
+
+impl<Func> MaybeMap<Func> for Nothing {
+    type Output = Nothing;
+}
+
+impl<Func, T> MaybeMap<Func> for Just<T>
+where
+    Func: Functor<T>,
+{
+    type Output = Just<ApplyFunctor<Func, T>>;
+}
+
+/// Filters a [Maybe] type by a [Functor].
+pub trait MaybeFilter<Func>
+where
+    Self: Maybe,
+    Self::Output: Maybe,
+{
+    type Output;
+}
+
+pub type MaybeFilterOutput<MaybeInput, Func> = <MaybeInput as MaybeFilter<Func>>::Output;
+
+impl<Func> MaybeFilter<Func> for Nothing {
+    type Output = Nothing;
+}
+
+impl<Func, T> MaybeFilter<Func> for Just<T>
+where
+    Func: Functor<T>,
+    Func::Output: Maybe,
+{
+    type Output = ApplyFunctor<Func, T>;
+}
+
+// tests
+
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::control::IfSameOutput;
-    use typenum::consts::*;
+    use crate::{
+        boolean::Boolean,
+        control::{IfElsePredicate, IfElsePredicateOutput, IfSameOutput},
+    };
+    use typenum::{consts::*, GrEq, IsGreaterOrEqual, Unsigned};
 
+    // unwrap
     type Opt1 = Just<U3>;
     type Opt2 = Nothing;
 
@@ -90,10 +144,41 @@ mod tests {
     type Assert2 = AssertSame<UnwrapOrOutput<Opt1, U0>, U3>;
     type Assert3 = AssertSame<UnwrapOrOutput<Opt2, U0>, U0>;
 
+    // map
+    struct BoxFunc;
+
+    impl<Input> Functor<Input> for BoxFunc {
+        type Output = Box<Input>;
+    }
+
+    type Assert4 = IfSameOutput<(), MaybeMapOutput<Just<i8>, BoxFunc>, Just<Box<i8>>>;
+    type Assert5 = IfSameOutput<(), MaybeMapOutput<Nothing, BoxFunc>, Nothing>;
+
+    // filter
+    struct ThresholdFunc;
+
+    impl<Input> Functor<Input> for ThresholdFunc
+    where
+        Input: Unsigned + IsGreaterOrEqual<U4>,
+        GrEq<Input, U4>: Boolean,
+        Just<Input>: IfElsePredicate<GrEq<Input, U4>, Nothing>,
+    {
+        type Output = IfElsePredicateOutput<Just<Input>, GrEq<Input, U4>, Nothing>;
+    }
+
+    type Assert6 = IfSameOutput<(), MaybeFilterOutput<Just<U8>, ThresholdFunc>, Just<U8>>;
+    type Assert7 = IfSameOutput<(), MaybeFilterOutput<Just<U2>, ThresholdFunc>, Nothing>;
+    type Assert8 = IfSameOutput<(), MaybeFilterOutput<Nothing, ThresholdFunc>, Nothing>;
+
     #[test]
     fn maybe_test() {
         let _: Assert1 = ();
         let _: Assert2 = ();
         let _: Assert3 = ();
+        let _: Assert4 = ();
+        let _: Assert5 = ();
+        let _: Assert6 = ();
+        let _: Assert7 = ();
+        let _: Assert8 = ();
     }
 }
