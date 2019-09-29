b480e32fb2bec37087cf811d1bbe0856b825f3f1
jerry73204 <jerry73204@gmail.com>
2019-09-30 05:04:25 +0800

    [kvlist] Split kvlist into several modules

---
+++ src/kvlist/access.rs
@@ -0,0 +1,148 @@
+use super::{KVCons, KVList};
+use crate::{
+    counter::{Counter, Current, Next},
+    functional::{ApplyFunctor, Functor},
+    list::{
+        LIndexOfManyOp, LIndexOfManyOpOutput, LIndexOfOp, LIndexOfOpOutput, LUnzipOp,
+        LUnzipOpFormerOutput, TList,
+    },
+    tuple::{SecondOf, SecondOfFunctor},
+};
+use std::marker::PhantomData;
+use typenum::Unsigned;
+
+/// A functor that gets index of `Target` in [KVList].
+pub struct KVIndexOfFunctor<Target, Index>
+where
+    Index: Counter,
+{
+    _phantom: PhantomData<(Target, Index)>,
+}
+
+pub type KVIndexOf<List, Target, Index> = ApplyFunctor<KVIndexOfFunctor<Target, Index>, List>;
+
+impl<List, Target, Index> Functor<List> for KVIndexOfFunctor<Target, Index>
+where
+    List: KVList + LUnzipOp,
+    Index: Counter,
+    LUnzipOpFormerOutput<List>: LIndexOfOp<Target, Index>,
+    LIndexOfOpOutput<LUnzipOpFormerOutput<List>, Target, Index>: Unsigned,
+{
+    type Output = LIndexOfOpOutput<LUnzipOpFormerOutput<List>, Target, Index>;
+}
+
+// index of many
+
+/// A functor that gets multiple indexes of `Targets` in [KVList].
+pub struct KVIndexOfManyFunctor<Targets, Indexes> {
+    _phantom: PhantomData<(Targets, Indexes)>,
+}
+
+impl<List, Targets, Indexes> Functor<List> for KVIndexOfManyFunctor<Targets, Indexes>
+where
+    List: KVList + LUnzipOp,
+    Targets: TList,
+    Indexes: TList,
+    LUnzipOpFormerOutput<List>: LIndexOfManyOp<Targets, Indexes>,
+    LIndexOfManyOpOutput<LUnzipOpFormerOutput<List>, Targets, Indexes>: TList,
+{
+    type Output = LIndexOfManyOpOutput<LUnzipOpFormerOutput<List>, Targets, Indexes>;
+}
+
+pub type KVIndexOfMany<List, Targets, Indexes> =
+    ApplyFunctor<KVIndexOfManyFunctor<Targets, Indexes>, List>;
+
+// get key-value pair
+
+/// A functor that gets key-value pair from [KVList].
+pub struct KVKeyValueAtFunctor<Target, Index> {
+    _phantom: PhantomData<(Target, Index)>,
+}
+
+pub type KVKeyValueAt<List, Target, Index> = ApplyFunctor<KVKeyValueAtFunctor<Target, Index>, List>;
+
+impl<Target, Value, Tail> Functor<KVCons<Target, Value, Tail>>
+    for KVKeyValueAtFunctor<Target, Current>
+where
+    Tail: KVList,
+{
+    type Output = (Target, Value);
+}
+
+impl<NonTarget, Value, Tail, Target, Index> Functor<KVCons<NonTarget, Value, Tail>>
+    for KVKeyValueAtFunctor<Target, Next<Index>>
+where
+    Tail: KVList,
+    Index: Counter,
+    KVKeyValueAtFunctor<Target, Index>: Functor<Tail>,
+{
+    type Output = KVKeyValueAt<Tail, Target, Index>;
+}
+
+// get value of key
+
+/// A functor that gets the value at `Target` in [KVList].
+pub struct KVValueAtFunctor<Target, Index>
+where
+    Index: Counter,
+{
+    _phantom: PhantomData<(Target, Index)>,
+}
+
+pub type KVValueAt<List, Target, Index> = ApplyFunctor<KVValueAtFunctor<Target, Index>, List>;
+
+impl<List, Target, Index> Functor<List> for KVValueAtFunctor<Target, Index>
+where
+    List: KVList,
+    Index: Counter,
+    KVKeyValueAtFunctor<Target, Index>: Functor<List>,
+    SecondOfFunctor: Functor<KVKeyValueAt<List, Target, Index>>,
+{
+    type Output = SecondOf<KVKeyValueAt<List, Target, Index>>;
+}
+
+// tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{control::IfSameOutput, KVListType, TListType};
+    use typenum::consts::*;
+
+    type AssertEqual<Lhs, Rhs> = IfSameOutput<(), Lhs, Rhs>;
+
+    struct A;
+    struct B;
+    struct C;
+
+    struct Va;
+    struct Vb;
+    struct Vc;
+
+    type SomeList = KVListType![(A, Va), (B, Vb), (C, Vc)];
+
+    // concat
+    type Assert13<Idx> =
+        AssertEqual<KVIndexOfMany<SomeList, TListType![C, A, B], Idx>, TListType![U2, U0, U1]>;
+
+    // index of
+    type Assert14<Idx> = AssertEqual<KVIndexOf<SomeList, A, Idx>, U0>;
+    type Assert15<Idx> = AssertEqual<KVIndexOf<SomeList, B, Idx>, U1>;
+    type Assert16<Idx> = AssertEqual<KVIndexOf<SomeList, C, Idx>, U2>;
+
+    // get key-value pair
+    type Assert17<Idx> = AssertEqual<KVKeyValueAt<SomeList, B, Idx>, (B, Vb)>;
+
+    // get value pair
+    type Assert18<Idx> = AssertEqual<KVValueAt<SomeList, B, Idx>, Vb>;
+
+    #[test]
+    fn kvlist_access_test() {
+        let _: Assert13<_> = ();
+        let _: Assert14<_> = ();
+        let _: Assert15<_> = ();
+        let _: Assert16<_> = ();
+        let _: Assert17<_> = ();
+        let _: Assert18<_> = ();
+    }
+}
---
+++ src/kvlist/misc.rs
@@ -0,0 +1,129 @@
+use super::KVList;
+use crate::{
+    functional::{ApplyFunctor, Functor},
+    list::{
+        LConcatOp, LConcatOpOutput, LLengthOp, LLengthOpOutput, LReverse, LReverseFunctor,
+        LSetEqualOp, LSetEqualOpOutput, LUnzipOp, LUnzipOpFormerOutput, TList,
+    },
+};
+use std::marker::PhantomData;
+use typenum::Unsigned;
+
+// length of list
+
+/// A functor that gets length of [KVList].
+pub struct KVLengthFunctor;
+
+impl<List> Functor<List> for KVLengthFunctor
+where
+    List: KVList + LLengthOp,
+    LLengthOpOutput<List>: Unsigned,
+{
+    type Output = LLengthOpOutput<List>;
+}
+
+pub type KVLength<List> = ApplyFunctor<KVLengthFunctor, List>;
+
+// reverse
+
+/// A [Functor] that reverses a [KVList].
+pub struct KVReverseFuntor {}
+
+pub type KVReverse<List> = ApplyFunctor<KVReverseFuntor, List>;
+
+impl<List> Functor<List> for KVReverseFuntor
+where
+    List: KVList,
+    LReverse<List>: TList,
+    LReverseFunctor: Functor<List>,
+{
+    type Output = LReverse<List>;
+}
+
+// set equal
+
+/// A functor that compares if two [KVList]s have same set of keys.
+pub struct KVSetEqualFuntor<Rhs, Indexes>
+where
+    Rhs: KVList,
+    Indexes: TList,
+{
+    _phantom: PhantomData<(Rhs, Indexes)>,
+}
+
+pub type KVSetEqual<Lhs, Rhs, Indexes> = ApplyFunctor<KVSetEqualFuntor<Rhs, Indexes>, Lhs>;
+
+impl<Lhs, Rhs, Indexes> Functor<Lhs> for KVSetEqualFuntor<Rhs, Indexes>
+where
+    Lhs: KVList + LUnzipOp,
+    Rhs: KVList + LUnzipOp,
+    Indexes: TList,
+    LUnzipOpFormerOutput<Lhs>: LSetEqualOp<LUnzipOpFormerOutput<Rhs>, Indexes>,
+{
+    type Output = LSetEqualOpOutput<LUnzipOpFormerOutput<Lhs>, LUnzipOpFormerOutput<Rhs>, Indexes>;
+}
+
+// concatenate
+
+/// A [Functor] that concatenates input and `Rhs` [KVList]s.
+pub struct KVConcatFunctor<Rhs>
+where
+    Rhs: KVList,
+{
+    _phantom: PhantomData<Rhs>,
+}
+
+pub type KVConcat<Lhs, Rhs> = ApplyFunctor<KVConcatFunctor<Rhs>, Lhs>;
+
+impl<Lhs, Rhs> Functor<Lhs> for KVConcatFunctor<Rhs>
+where
+    Lhs: KVList + LConcatOp<Rhs>,
+    Rhs: KVList,
+    LConcatOpOutput<Lhs, Rhs>: KVList,
+{
+    type Output = LConcatOpOutput<Lhs, Rhs>;
+}
+
+// tests
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{control::IfSameOutput, KVListType, TListType};
+    use typenum::consts::*;
+
+    type AssertEqual<Lhs, Rhs> = IfSameOutput<(), Lhs, Rhs>;
+
+    struct A;
+    struct B;
+    struct C;
+    struct D;
+    struct E;
+
+    struct Va;
+    struct Vb;
+    struct Vc;
+    struct Vd;
+    struct Ve;
+
+    type SomeList = KVListType![(A, Va), (B, Vb), (C, Vc)];
+    type AnotherList = KVListType![(D, Vd), (E, Ve)];
+    // reverse list
+    type Assert10 = AssertEqual<KVReverse<SomeList>, KVListType![(C, Vc), (B, Vb), (A, Va)]>;
+
+    // assert identical set of items
+    type Assert11<Idx> = KVSetEqual<SomeList, KVListType![(C, Vc), (A, Va), (B, Vb)], Idx>;
+
+    // concat
+    type Assert12 = AssertEqual<
+        KVConcat<SomeList, AnotherList>,
+        KVListType![(A, Va), (B, Vb), (C, Vc), (D, Vd), (E, Ve)],
+    >;
+
+    #[test]
+    fn kvlist_misc_test() {
+        let _: Assert10 = ();
+        let _: Assert11<_> = ();
+        let _: Assert12 = ();
+    }
+}
