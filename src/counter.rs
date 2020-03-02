fb18ee04acdf900b3e71fb8bace404a8690147b2
jerry73204 <jerry73204@gmail.com>
2020-03-02 18:13:19 +0800

    [counter] Implement Counter from/to Unsigned conversion

--- src/counter.rs
+++ src/counter.rs
@@ -74,15 +74,127 @@
 //! ```
 
 use crate::list::base::{Cons, List, Nil};
+use std::ops::{Add, Sub};
+use typenum::{Add1, Bit, Sub1, UInt, UTerm, Unsigned, B0, B1};
 
-pub trait Counter
-where
-    Self: List,
-{
+pub mod base {
+    use super::*;
+
+    pub trait Counter
+    where
+        Self: List,
+    {
+    }
+
+    pub type Step<Tail> = Cons<(), Tail>;
+
+    impl<Tail> Counter for Step<Tail> where Tail: Counter {}
+
+    impl Counter for Nil {}
 }
 
-pub type Step<Tail> = Cons<(), Tail>;
+pub use base::*;
 
-impl<Tail> Counter for Step<Tail> where Tail: Counter {}
+pub mod ops {
+    use super::*;
 
-impl Counter for Nil {}
+    // counter to unsigned
+
+    pub trait ToUnsigned
+    where
+        Self: Counter,
+        Self::Output: Unsigned,
+    {
+        type Output;
+    }
+
+    impl ToUnsigned for Nil {
+        type Output = UTerm;
+    }
+
+    impl<Next> ToUnsigned for Step<Next>
+    where
+        Next: Counter + ToUnsigned,
+        op_aliases::ToUnsigned<Next>: Add<B1>,
+        Add1<op_aliases::ToUnsigned<Next>>: Unsigned,
+    {
+        type Output = Add1<op_aliases::ToUnsigned<Next>>;
+    }
+
+    // unsigned to counter
+
+    pub trait ToCounter
+    where
+        Self: Unsigned,
+        Self::Output: Counter,
+    {
+        type Output;
+    }
+
+    impl ToCounter for UTerm {
+        type Output = Nil;
+    }
+
+    impl ToCounter for UInt<UTerm, B1> {
+        type Output = Step<Nil>;
+    }
+
+    impl<U, B> ToCounter for UInt<UInt<U, B>, B1>
+    where
+        U: Unsigned,
+        B: Bit,
+        UInt<UInt<U, B>, B0>: ToCounter,
+    {
+        type Output = Step<op_aliases::ToCounter<UInt<UInt<U, B>, B0>>>;
+    }
+
+    impl<U> ToCounter for UInt<U, B0>
+    where
+        U: Unsigned + Sub<B1>,
+        UInt<Sub1<U>, B1>: ToCounter,
+    {
+        type Output = Step<op_aliases::ToCounter<UInt<Sub1<U>, B1>>>;
+    }
+}
+
+pub mod op_aliases {
+    use super::*;
+
+    pub type ToUnsigned<Count> = <Count as ops::ToUnsigned>::Output;
+    pub type ToCounter<Value> = <Value as ops::ToCounter>::Output;
+}
+
+pub mod macros {
+    #[macro_export]
+    macro_rules! CounterT {
+        ($size:ty) => { $crate::counter::op_aliases::ToCounter<$size> };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{op_aliases::*, *};
+    use crate::{control::op_aliases::*, CounterT};
+    use typenum::{U0, U1, U2, U3};
+
+    type Assert1 = AssertSame<CounterT![U0], Nil, ()>;
+    type Assert2 = AssertSame<CounterT![U1], Step<Nil>, ()>;
+    type Assert3 = AssertSame<CounterT![U2], Step<Step<Nil>>, ()>;
+    type Assert4 = AssertSame<CounterT![U3], Step<Step<Step<Nil>>>, ()>;
+    type Assert5 = AssertSame<ToUnsigned<Nil>, U0, ()>;
+    type Assert6 = AssertSame<ToUnsigned<Step<Nil>>, U1, ()>;
+    type Assert7 = AssertSame<ToUnsigned<Step<Step<Nil>>>, U2, ()>;
+    type Assert8 = AssertSame<ToUnsigned<Step<Step<Step<Nil>>>>, U3, ()>;
+
+    #[test]
+    fn counter_test() {
+        let _: Assert1 = ();
+        let _: Assert2 = ();
+        let _: Assert3 = ();
+        let _: Assert4 = ();
+        let _: Assert5 = ();
+        let _: Assert6 = ();
+        let _: Assert7 = ();
+        let _: Assert8 = ();
+    }
+}
