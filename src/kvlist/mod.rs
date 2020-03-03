4a7f09b60bf9e7dadf045c9bca57afd69d2db5af
jerry73204 <jerry73204@gmail.com>
2020-03-03 18:07:54 +0800

    [kvlist] Update mod list

--- src/kvlist/mod.rs
+++ src/kvlist/mod.rs
@@ -1,17 +1,8 @@
 //! A typed list of key-value pairs.
 
-// mod access;
 mod base;
-mod operators;
-// mod insert;
 mod macros;
-// pub mod marker;
-// mod misc;
-// mod remove;
+mod operators;
 
-// pub use access::*;
 pub use base::*;
 pub use operators::*;
-// pub use insert::*;
-// pub use misc::*;
-// pub use remove::*;
