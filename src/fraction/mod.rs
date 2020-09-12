ddbcb1b2c42ae4596ae18fb6c3a9674bb80c47f7
jerry73204 <jerry73204@gmail.com>
2020-09-13 07:44:51 +0800

    Tidy fraction module files

--- src/fraction/mod.rs
+++ src/fraction/mod.rs
@@ -1,7 +1,6 @@
 pub mod macros;
 pub mod marker;
-// mod operations;
-mod ops;
+pub mod ops;
 pub mod signed;
 pub mod unsigned;
 
@@ -9,5 +8,3 @@ pub use marker::*;
 pub use ops::*;
 pub use signed::*;
 pub use unsigned::*;
-
-// pub use operations::{op_aliases, ops};
