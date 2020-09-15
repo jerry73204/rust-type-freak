4c2d33dde2f97b103113c7b8de3e6630930347a3
jerry73204 <jerry73204@gmail.com>
2020-09-15 10:37:03 +0800

    Minor edit on mod visibility

--- src/floating/mod.rs
+++ src/floating/mod.rs
@@ -1,6 +1,6 @@
-pub mod base;
-pub mod marker;
-pub mod ops;
+mod base;
+mod marker;
+mod ops;
 
 pub use base::*;
 pub use marker::*;
--- src/fraction/mod.rs
+++ src/fraction/mod.rs
@@ -1,8 +1,8 @@
-pub mod macros;
-pub mod marker;
-pub mod ops;
-pub mod signed;
-pub mod unsigned;
+mod macros;
+mod marker;
+mod ops;
+mod signed;
+mod unsigned;
 
 pub use marker::*;
 pub use ops::*;
